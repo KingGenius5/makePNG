e77426b685c46ebfd7910e9ee221b6524b1fc4a1
Muhammad Tifak <moht20009@gmail.com>
2021-03-06 01:54:12 -0500

    Changed syntax + wrapped up funcs

--- main.go
+++ main.go
@@ -11,31 +11,34 @@ import (
 
 func main() {
 
-	// Start Chrome
-	// Remove the 2nd param if you don't need debug information logged
+	// Starting Chrome
 	ctx, cancel := chromedp.NewContext(context.Background(), chromedp.WithDebugf(log.Printf))
 	defer cancel()
 
+	// Want to turn these into command line args using flag
 	url := "https://www.nytimes.com/"
 	filename := "nyt.png"
 
 	// Run Tasks
 	// List of actions to run in sequence (which also fills our image buffer)
 	var imageBuf []byte
-	if err := chromedp.Run(ctx, ScreenshotTasks(url, &imageBuf)); err != nil {
+	if err := chromedp.Run(ctx, TakeScreenshot(url, &imageBuf)); err != nil {
 		log.Fatal(err)
 	}
 
-	// Write our image to file
+	// This writes the image to file
 	if err := ioutil.WriteFile(filename, imageBuf, 0644); err != nil {
 		log.Fatal(err)
 	}
 }
 
-func ScreenshotTasks(url string, imageBuf *[]byte) chromedp.Tasks {
+func TakeScreenshot(url string, imageBuf *[]byte) chromedp.Tasks {
 	return chromedp.Tasks{
 		chromedp.Navigate(url),
 		chromedp.ActionFunc(func(ctx context.Context) (err error) {
+
+			// Cool little addition, if I wanted to make a PDF instead of a PNG I'd change this line to
+			// *imageBuf, _, err = page.PrintToPDF().WithPrintBackground(false).Do(ctx)
 			*imageBuf, err = page.CaptureScreenshot().WithQuality(90).Do(ctx)
 			return err
 		}),
